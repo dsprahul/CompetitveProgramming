fc666b3d9fb90cfacff66cdc39dc1da71a58f7f3
Rahul Damineni <rahul.damineni@themathcompany.com>
2018-12-26 13:46:53 +0530

    Skipped checking subspace from the beginning by resuing last match index

--- 6_variation_count.py
+++ 6_variation_count.py
@@ -4,19 +4,18 @@ list_ = map(int, input().split())
 
 
 # 1. Sort the list in descending order to avoid using mod operator
-list_ = sorted(list_, reverse=True)
+list_ = sorted(list_)
 
 # 2. Starting from first element, compare along the length of the list
 #    to count number of pairs satisfying |i-j| >= K
 
 variation_count = 0
-for id_, element in enumerate(list_):
-    if id_ + 1 == N - 1:
-        continue
-
-    for p, element_along_len in enumerate(list_[id_ + 1:]):
-        if element - element_along_len >= K:
-            variation_count += (N - 1) - (id_ + p)
+S = 1
+for i in range(N):
+    for j in range(S, N):
+        if list_[j] - list_[i] >= K:
+            variation_count += N - j
+            S = j
             break
 
 print(variation_count)
