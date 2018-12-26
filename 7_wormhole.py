dafe2d0345cbf2bfad8bd49c4bc66572ca868e8d
Rahul Damineni <rahul.damineni@themathcompany.com>
2018-12-26 16:48:05 +0530

    Solution incorrect in most cases and timing out in some others

--- 7_wormhole.py
+++ 7_wormhole.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from bisect import bisect
+from itertools import product
 
 # Know scale of inputs
 N, X, Y = map(int, input().split())
@@ -24,9 +26,12 @@ def minimum_time_spent(exams, W, V):
     V.sort()
 
     def next_smallest_exam_set(sorted_known_dts, current_dt):
-        for idx, dt in enumerate(sorted_known_dts):
-            if dt > current_dt:
-                return exams_by_duration[sorted_known_dts[idx - 1]]
+        these_exams = []
+        idx = bisect(sorted_known_dts, current_dt)
+        for dt_key in sorted_known_dts[:idx]:
+            these_exams += exams_by_duration[dt_key]
+
+        return these_exams
 
     for w, v in product(W, V):
         dt = v - w
@@ -34,24 +39,19 @@ def minimum_time_spent(exams, W, V):
         if dt < 0:  # Such combinations are not commutable
             continue
 
-        if exams_by_duration.get(dt) is None:
-            try:
-                existing_dts_ascending = sorted(list(exams_by_duration.keys()))
-                these_exams = next_smallest_exam_set(
-                    sorted_known_dts=existing_dts_ascending,
-                    current_dt=dt
-                )
-            except IndexError:
-                continue
-        else:
-            these_exams = exams_by_duration[dt]
+        # There maybe some exams that could fit in this duration,
+        # check if their start & end time lies with-in <dt>
+        existing_dts_ascending = sorted(list(exams_by_duration.keys()))
+        in_range_exams = next_smallest_exam_set(
+            sorted_known_dts=existing_dts_ascending,
+            current_dt=dt
+        )
 
-        # There are some exams that could fit in this duration,
-        # check if the start & end time lies with-in <dt>
-        for s, e in these_exams:  # If exits without returing, try next dt combo
+        # If exits without returing, try next dt combo
+        for s, e in in_range_exams:
             if s + dt <= v:
                 return dt + 1
 
 
 if __name__ == "__main__":
-    minimum_time_spent(exams, W, V)
+    print(minimum_time_spent(exams, W, V))
