2275eed40f9a99bdbd3843567c1d8fdc853ae6c5
Rahul Damineni <rahul.damineni@themathcompany.com>
2019-01-03 14:51:01 +0530

    [Wormhole] So far, I was looking into one possibility only. Which among N^2 combinations of W & V times can have an exam fitted b/w them? This required at least N^2 + kNlog(N) iterations in the worst case. Although best case could be much smaller (Could be < N).
    Now I explored another possibility, Go through all N exams and find commute time for each of them using binary
    search on W & V (logN each). This solution's complexity would be like: N + 2NlogN + 2logN
    Best/worst case it will run N times

--- 7_wormhole.py
+++ 7_wormhole.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
-from bisect import bisect
-from itertools import product
+from bisect import bisect_right, bisect_left
 
 # Know scale of inputs
 N, X, Y = map(int, input().split())
@@ -14,96 +12,28 @@ for i in range(N):
 W = list(map(int, input().split()))
 V = list(map(int, input().split()))
 
+W.sort()
+V.sort()
 
-class BinaryTable(object):
+minW = min(W)
+maxV = max(V)
 
-    def __init__(self):
-        self.table = defaultdict(dict)
+minimum_time = 10**5 + 1
 
-    def set(self, s, dt):
-        self.table[s].update({dt: 1})
+for s, e in exams:
 
-    def get(self, s, dt):
-        if self.table.get(s) is None:
-            return 0
+    if s < minW or e > maxV:
+        continue
 
-        if self.table[s].get(dt) is None:
-            return 0
+    insertion_idx = bisect_right(W, s)
+    if insertion_idx < 0:
+        continue
+    to_wormhole_time = W[insertion_idx - 1]
 
-        return self.table[s][dt]
+    insertion_idx = bisect_left(V, e)
+    from_wormhole_time = V[insertion_idx]
 
-    def load_all_start_times(self):
-        self.start_times = self.table.keys()
+    if from_wormhole_time - to_wormhole_time < minimum_time:
+        minimum_time = from_wormhole_time - to_wormhole_time
 
-    def load_all_dts(self):
-        self.dts = []
-        for s in self.start_times:
-            self.dts += self.table[s].keys()
-
-    @property
-    def get_all_start_times(self):
-        try:
-            type(self.start_times)
-            type(self.dts)
-        except AttributeError:
-            self.load_all_start_times()
-            self.load_all_dts()
-
-        return self.start_times
-
-    @property
-    def get_all_dts(self):
-        try:
-            type(self.start_times)
-            type(self.dts)
-        except AttributeError:
-            self.load_all_start_times()
-            self.load_all_dts()
-
-        return self.dts
-
-
-def minimum_time_spent(exams, W, V):
-
-    # exams = sorted(exams, key=lambda i_: i_[0])
-    bt = BinaryTable()
-    for s, e in exams:
-        bt.set(s=s, dt=e-s)
-
-    product_WV = product(W, V)
-    product_WV = sorted(product_WV, key=lambda exam: exam[1] - exam[0])
-
-    def next_smallest_exam_set(sorted_known_dts, current_dt):
-        these_exams = []
-        idx = bisect(sorted_known_dts, current_dt)
-        for dt_key in sorted_known_dts[:idx]:
-            these_exams += exams_by_duration[dt_key]
-
-        return these_exams
-
-    def in_range_starts(sorted_s, start_s, end_s):
-        return [e for e in sorted_s if start_s >= e <= end_s]
-
-    def in_range_dts(sorted_dt, max_dt):
-        return [e for e in sorted_dt if e <= max_dt]
-
-    for w, v in product_WV:
-        dt = v - w
-
-        if dt < 0:  # Such combinations are not commutable
-            continue
-
-        known_s = sorted(bt.get_all_start_times)
-        known_dt = sorted(bt.get_all_dts)
-        # There maybe some exams that could fit in this duration,
-        # check if their start & end time lies with-in <dt>
-        # Get a list of exams such that w <= s < w + dt && t <= dt
-        starts = in_range_starts(known_s, start_s=w, end_s=w + dt)
-        dts = in_range_dts(known_dt, max_dt=dt)
-        for s, dt2 in product(starts, dts):
-            if bt.get(s, dt2) == 1:
-                return dt + 1
-
-
-if __name__ == "__main__":
-    print(minimum_time_spent(exams, W, V))
+print(minimum_time + 1)
