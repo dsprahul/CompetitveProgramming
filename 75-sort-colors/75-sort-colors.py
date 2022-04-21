61677f0aae0c4d7f3febdfbed5ef47cb6dc985ac
Rahul Damineni <daminenirahul@gmail.com>
2022-04-20 22:08:29 -0700

    Time: 61 ms (18.44%), Space: 14 MB (16.61%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,40 +4,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        n = len(nums)
-        sorter = 0
-        runner = 0
+        n0, n1, n2 = 0, 0, 0
         
-        current_color = 0
-        
-        while sorter < n - 1:
-            
-            # Reset bubbled/runner after each runner pass
-            runner = sorter
-            
-            # First unsorted ball
-            if nums[sorter] != current_color:
-                
-                while runner < n:
-                    # Discovered a element that should be at current sorter
-                    if nums[runner] == current_color:
-                        bubble = True
-                        break    
-                    runner += 1
-                else:
-                    bubble = False
-                    current_color += 1
-                    
-                # bubble things to right by one place
-                if bubble:
-                    while runner > sorter:
-                        nums[runner] = nums[runner - 1]
-                        runner -= 1
-                    nums[runner] = current_color
-                    sorter += 1
-
-
+        for i in nums:
+            if i == 0:
+                n0 += 1
+            elif i == 1:
+                n1 += 1
             else:
-                sorter += 1
-
+                n2 += 1
+                
+        for i in range(len(nums)):
+            if i < n0:
+                nums[i] = 0
+            elif n0 <= i < n0 + n1:
+                nums[i] = 1
+            else:
+                nums[i] = 2
             
